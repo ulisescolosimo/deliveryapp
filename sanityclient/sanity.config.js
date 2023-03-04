d565ad1a60601c4d0961e4adce3267e9347d2d53
Ulises Colosimo <colosimo101@gmail.com>
2023-03-04 13:51:39 -0300

    Added sanity

---
+++ sanityclient/sanity.config.js
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'delivery-native',
+
+  projectId: '4ezjea62',
+  dataset: 'production',
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
